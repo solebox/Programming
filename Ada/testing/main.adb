724e527764c08e38e7b771da548464f4d720c200
jake <solekiller@gmail.com>
2012-01-07 06:23:04 +0200

    woot

--- Ada/testing/main.adb
+++ Ada/testing/main.adb
@@ -3,13 +3,23 @@ with Text_IO; use Text_IO;
 Procedure main is
    type Month is (jan,feb,mar,apr,may,jun,jul,aug,sep,oct,nov,dec);
    type Year is array(Positive range 1..12) of Month;
+   type StringPtr is access String;
+   type i_acc is access integer;
    yea:Year;
    str:string(1..55);
    len:integer;
+    function foo(S:String) return String is
+        stpt:StringPtr;
+        i:i_acc;
+    begin
+       stpt := new String'(S&S&S); 
+        return stpt.all;
+    end foo;
 begin
     get_line(str,len);
-    for i in 1..len loop
-        put(str(i));
-    end loop;
+        put(foo(str(1..len)));
    new_line;
+   for i in 1..1 loop
+       put_line("wazzzzzzza");
+   end loop;
 end main;
