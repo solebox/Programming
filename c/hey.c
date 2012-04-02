8f37552c3163fd684db27a4b50b09f05e6374e76
Jacob <solekiller@5013box@gmail.com>
2012-04-02 13:02:56 +0300

    finished making the DD menu

--- c/hey.c
+++ c/hey.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
+unsigned int integer = 0;
+unsigned long longint = 0;
+unsigned char character = 0;
 int main(void)
 {
+    srand(80); 
+    printf("%d \n",rand());
+    printf("%d \n",rand());
+    printf("%d \n",rand());
+    printf("%u \n",sizeof(long int));
+    printf("%u \n",sizeof(char));
 	printf("hello world\n");
 	return 0;
 }
